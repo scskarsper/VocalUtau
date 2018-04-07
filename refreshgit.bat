f058f5980df52f88462559cf51de1fb706f78086
MISATOSUSUMI <scskarsper@qq.com>
2018-04-07 19:03:11 +0800

    （20180407190308）

---
+++ refreshgit.bat
@@ -0,0 +1,25 @@
+@echo off
+@echo ===========================
+@echo VocalUtau Main
+@cd D:\VocalUtau\VocalUtau
+@git pull
+
+@echo ===========================
+@echo VocalUtau DirectUI
+@cd D:\VocalUtau\IncludeLib\VocalUtau.DirectUI
+@git pull
+
+@echo ===========================
+@echo VocalUtau Formats
+@cd D:\VocalUtau\IncludeLib\VocalUtau.Formats
+@git pull
+
+@echo ===========================
+@echo VocalUtau Calculators
+@cd D:\VocalUtau\IncludeLib\VocalUtau.Calculators
+@git pull
+
+@echo ===========================
+@echo VocalUtau WavTools
+@cd D:\VocalUtau\IncludeLib\VocalUtau.WavTools
+@git pull
---
+++ resetgit.bat
@@ -0,0 +1,30 @@
+@echo off
+@echo ===========================
+@echo VocalUtau Main
+@cd D:\VocalUtau\VocalUtau
+@git fetch --all
+@git reset --hard origin/masterss
+
+@echo ===========================
+@echo VocalUtau DirectUI
+@cd D:\VocalUtau\IncludeLib\VocalUtau.DirectUI
+@git fetch --all
+@git reset --hard origin/masterss
+
+@echo ===========================
+@echo VocalUtau Formats
+@cd D:\VocalUtau\IncludeLib\VocalUtau.Formats
+@git fetch --all
+@git reset --hard origin/masterss
+
+@echo ===========================
+@echo VocalUtau Calculators
+@cd D:\VocalUtau\IncludeLib\VocalUtau.Calculators
+@git fetch --all
+@git reset --hard origin/masterss
+
+@echo ===========================
+@echo VocalUtau WavTools
+@cd D:\VocalUtau\IncludeLib\VocalUtau.WavTools
+@git fetch --all
+@git reset --hard origin/masterss
