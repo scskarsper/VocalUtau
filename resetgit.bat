2d0e986a423937baabbfa66d8b074657dd259a1d
scskarsper <scskarsper@qq.com>
2018-04-10 17:41:48 +0800

    （20180410174147）

--- resetgit.bat
+++ resetgit.bat
@@ -3,28 +3,28 @@
 @echo VocalUtau Main
 @cd D:\VocalUtau\VocalUtau
 @git fetch --all
-@git reset --hard origin/masterss
+@git reset --hard origin/master
 
 @echo ===========================
 @echo VocalUtau DirectUI
 @cd D:\VocalUtau\IncludeLib\VocalUtau.DirectUI
 @git fetch --all
-@git reset --hard origin/masterss
+@git reset --hard origin/master
 
 @echo ===========================
 @echo VocalUtau Formats
 @cd D:\VocalUtau\IncludeLib\VocalUtau.Formats
 @git fetch --all
-@git reset --hard origin/masterss
+@git reset --hard origin/master
 
 @echo ===========================
 @echo VocalUtau Calculators
 @cd D:\VocalUtau\IncludeLib\VocalUtau.Calculators
 @git fetch --all
-@git reset --hard origin/masterss
+@git reset --hard origin/master
 
 @echo ===========================
 @echo VocalUtau WavTools
 @cd D:\VocalUtau\IncludeLib\VocalUtau.WavTools
 @git fetch --all
-@git reset --hard origin/masterss
+@git reset --hard origin/master
