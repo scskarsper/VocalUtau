ed1d3c52ea31ac2b84717bf1b69f5db31b0da1ec
scskarsper <scskarsper@qq.com>
2018-04-12 21:56:25 +0800

    Add files via upload
    cloneall

---
+++ cloneallgit.bat
@@ -0,0 +1,44 @@
+@REM Base GIT TLS£ºhttps://github.com/Microsoft/Git-Credential-Manager-for-Windows/releases/download/v1.14.0/GCMW-1.14.0.exe
+@echo off
+@echo ===========================
+@echo VocalUtau Main
+@mkdir D:\VocalUtau
+@cd D:\VocalUtau
+@D:
+@git clone https://github.com/scskarsper/VocalUtau.git
+
+@echo ===========================
+@echo VocalUtau DirectUI
+@mkdir D:\VocalUtau\IncludeLib
+@cd D:\VocalUtau\IncludeLib
+@D:
+@git clone https://github.com/scskarsper/VocalUtau.DirectUI.git
+
+@echo ===========================
+@echo VocalUtau Formats
+@mkdir D:\VocalUtau\IncludeLib
+@cd D:\VocalUtau\IncludeLib
+@D:
+@git clone https://github.com/scskarsper/VocalUtau.Formats.git
+
+@echo ===========================
+@echo VocalUtau Calculators
+@mkdir D:\VocalUtau\IncludeLib
+@cd D:\VocalUtau\IncludeLib
+@D:
+@git clone https://github.com/scskarsper/VocalUtau.Calculators.git
+
+@echo ===========================
+@echo VocalUtau WavTools
+@mkdir D:\VocalUtau\IncludeLib
+@cd D:\VocalUtau\IncludeLib
+@D:
+@git clone https://github.com/scskarsper/VocalUtau.WavTools.git
+
+
+@echo ===========================
+@echo VocalUtau WavTools
+@mkdir D:\VocalUtau
+@cd D:\VocalUtau
+@D:
+@git clone https://github.com/scskarsper/VocalUtau.DebugExampleFiles.git
